b9a00502e1d4201b06ca8094b532d1542633e404
Darlan de Moraes Silveira <darlan@live.in>
2017-02-07 16:43:38 -0200

    A more simple approach

--- src/test/java/com/boot/spring/AppBootTests.java
+++ src/test/java/com/boot/spring/AppBootTests.java
@@ -1,4 +1,4 @@
-package com.zenvia;
+package com.boot.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootVertxBenchmarkApplicationTests {
+public class AppBootTests {
 
 	@Test
 	public void contextLoads() {
